496ad98b6c2cb754849233afd25ae681ee54f9cf
tuan2239 <51068642+tuan2239@users.noreply.github.com>
2026-07-21 10:20:42 +0700

    Add: top-k-elements-in-list - submission-3

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-3.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int[] TopKFrequent(int[] nums, int k) {
+        int[] arr = new int[k];
+        Dictionary<int, int> dict = new Dictionary<int, int>();
+        foreach(var data in nums){
+            if(dict.ContainsKey(data)){
+                dict[data]++;
+            }
+            else{
+                dict[data]=1;
+            }
+        }
+        var heap= new PriorityQueue<int, int>();
+        foreach(var data in dict){
+            heap.Enqueue(data.Key, -data.Value);
+        }
+        for(int i =0; i<k ; i++){
+            arr[i] = heap.Dequeue();
+        }
+        return arr;
+    }
+}
